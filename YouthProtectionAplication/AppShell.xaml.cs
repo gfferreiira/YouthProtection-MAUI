3fb32c39942a79f85036e0262957d9f9a4071fc1
gfferreiira <arzhun7381@gmail.com>
2024-08-21 18:28:02 -0300

    Chamada na tela login para a tela de cadastro atráves do button 'criar conta'

--- YouthProtectionAplication/AppShell.xaml.cs
+++ YouthProtectionAplication/AppShell.xaml.cs
@@ -1,10 +1,14 @@
-﻿namespace YouthProtectionAplication
+﻿using YouthProtectionAplication.Views;
+
+namespace YouthProtectionAplication
 {
     public partial class AppShell : Shell
     {
         public AppShell()
         {
             InitializeComponent();
+
+            Routing.RegisterRoute(nameof(CreateAccountPage), typeof(CreateAccountPage));
         }
     }
 }
