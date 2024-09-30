9ec371945425cae97baac15efc83716e61b336e3
Guilherme Ferreira Oliveira <144188616+gfferreiira@users.noreply.github.com>
2024-09-30 13:27:11 -0300

    Update Request.cs

--- YouthProtectionAplication/Services/Request.cs
+++ YouthProtectionAplication/Services/Request.cs
@@ -35,5 +35,51 @@ namespace YouthProtectionAplication.Services
                 result = await Task.Run(() => JsonConvert.DeserializeObject<TResult>(serialized));
             return result;
         }
+         public async Task<int> DeleteAsync(string uri, string token)
+        {
+            HttpClient httpClient = new HttpClient();
+            httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer",
+           token);
+            HttpResponseMessage response = await httpClient.DeleteAsync(uri);
+            string serialized = await response.Content.ReadAsStringAsync();
+            if (response.StatusCode == System.Net.HttpStatusCode.OK)
+                return int.Parse(serialized);
+            else
+                throw new Exception(serialized);
+        }
+
+        public async Task<TResult> GetAsync<TResult>(string uri, string token)
+        {
+            HttpClient httpClient = new HttpClient();
+            httpClient.DefaultRequestHeaders.Authorization
+            = new AuthenticationHeaderValue("Bearer", token);
+            HttpResponseMessage response = await httpClient.GetAsync(uri);
+            string serialized = await response.Content.ReadAsStringAsync();
+
+            if (response.StatusCode != System.Net.HttpStatusCode.OK)
+                throw new Exception(serialized);
+            TResult result = await Task.Run(() => JsonConvert.DeserializeObject<TResult>(serialized));
+            return result;
+        }
+
+        public async Task<int> PutAsync<TResult>(string uri, TResult data, string token)
+        {
+            HttpClient httpClient = new HttpClient();
+            httpClient.DefaultRequestHeaders.Authorization
+            = new AuthenticationHeaderValue("Bearer", token);
+            var content = new StringContent(JsonConvert.SerializeObject(data));
+            content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+            HttpResponseMessage response = await httpClient.PutAsync(uri, content);
+            string serialized = await response.Content.ReadAsStringAsync();
+            if (response.StatusCode == System.Net.HttpStatusCode.OK)
+                return int.Parse(serialized);
+            else
+                throw new Exception(serialized);
+        }
+
+        
+
+        
+        
     }
 }
