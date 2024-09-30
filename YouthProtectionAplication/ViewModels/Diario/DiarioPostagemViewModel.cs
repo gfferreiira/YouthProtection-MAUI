266dd722c5e8f26ff9773df0600306b49fcebe1f
Guilherme Ferreira Oliveira <144188616+gfferreiira@users.noreply.github.com>
2024-09-30 11:04:02 -0300

    Update DiarioPostagemViewModel.cs

--- YouthProtectionAplication/ViewModels/Diario/DiarioPostagemViewModel.cs
+++ YouthProtectionAplication/ViewModels/Diario/DiarioPostagemViewModel.cs
@@ -16,6 +16,8 @@ namespace YouthProtectionAplication.ViewModels.Diario
         {
             string token = Preferences.Get("UsuarioToken", string.Empty);
             UserDiarioService = new DiarioService(token);
+
+            _ = ObterTipoPostagem();
         }
 
         private int idUser;
@@ -23,6 +25,7 @@ namespace YouthProtectionAplication.ViewModels.Diario
         private string titulo = string.Empty;
         private string texto = string.Empty;
         private DateTime dataPostagem;
+        private ObservableCollection<TipoPostagemEnum> listaTipoPostagem;
 
         #region Atributos Propriedades
         public int IdUser 
@@ -51,7 +54,49 @@ namespace YouthProtectionAplication.ViewModels.Diario
             set => dataPostagem = value;
         }
 
+         public ObservableCollection<TipoPostagemEnum> listaTipoPostagem
+        {
+            get { return listaTipoPostagem; }
+            set
+             {
+                 if (value != null)
+                 {
+                     listaTipoPostagem = value;
+                     OnPropertyChanged();
+                }
+            }
+        }
+
 
         #endregion
-    }
+
+        #region Métodos 
+    
+        public async Task ObterTipoPostagem()
+        {
+            try
+            {
+                ListaTipoPostagem = new ObservableCollection<TipoPostagemEnum>();
+                ListaTipoPostagem.Add(new TipoPostagemEnum() {Id = 0, Descricao = "Público" });
+                ListaTipoPostagem.Add(new TipoPostagemEnum() {Id = 1, Descricao = "Privado" });
+                OnPropertyChanged(nameof(ListaTipoPostagem));
+            }
+             catch (Exception ex)
+            {
+                await Application.Current.MainPage
+                    .DisplayAlert("Ops", ex.Message + " Detalhes: " + ex.InnerException, "Ok");
+            }
+         }
+                
+
+        
+        #endregion
+        
+
+
+        
+
+       
+
+        
 }
